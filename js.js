31b5113cca178b999fc5c64883e78493a4ad123a
henkwillem <48722167+henkwillem@users.noreply.github.com>
2020-01-16 20:32:31 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var verzendknop = document.querySelector("button");
+var tekstje = document.querySelector("div h2");
+
+verzendknop.addEventListener("click", function() {
+    tekstje.classList.toggle("joejoe");
+})
+
+
+
+
+
+
